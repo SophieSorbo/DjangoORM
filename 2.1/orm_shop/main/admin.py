e6a74523063fafbc3d9906c6d6fb42a3603754a1
Sophie <sophiesorbo@gmail.com>
2024-04-03 11:36:09 +0300

    first

--- 2.1/orm_shop/main/admin.py
+++ 2.1/orm_shop/main/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# зарегистрируйте необходимые модели
+from main import models
+
+
+admin.site.register(models.Car)
+admin.site.register(models.Client)
+admin.site.register(models.Sale)
--- 2.1/orm_shop/main/views.py
+++ 2.1/orm_shop/main/views.py
@@ -1,25 +1,31 @@
 from django.http import Http404
 from django.shortcuts import render
-
-from main.models import Car
+from .models import Car, Client, Sale
 
 
 def cars_list_view(request):
-    # получите список авто
     template_name = 'main/list.html'
-    return render(request, template_name, {})  # передайте необходимый контекст
+    cars = Car.objects.all()
+    return render(request, template_name, context={'cars': cars})  # передайте необходимый контекст
 
 
 def car_details_view(request, car_id):
     # получите авто, если же его нет, выбросьте ошибку 404
-    template_name = 'main/details.html'
-    return render(request, template_name, {})  # передайте необходимый контекст
+    try:
+        template_name = 'main/details.html'
+        car = Car.objects.get(id=car_id)
+        return render(request, template_name, {'car': car})  # передайте необходимый контекст
+    except Car.DoesNotExist:
+        raise Http404('Car not found')
+
 
 
 def sales_by_car(request, car_id):
     try:
-        # получите авто и его продажи
+        car = Car.objects.get(id=car_id)
+        sales = Sale.objects.filter(car=car)
         template_name = 'main/sales.html'
-        return render(request, template_name, {})  # передайте необходимый контекст
+        context = {'car': car, 'sales': sales}
+        return render(request, template_name, context)
     except Car.DoesNotExist:
         raise Http404('Car not found')
